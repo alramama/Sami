3713587a80ebf6a6ec6f5f31b72a0c12996f93e0
alramama <74817864+alramama@users.noreply.github.com>
2022-02-11 22:19:01 +0300

    Update and rename LOGIN_USER.php to login_mysql.php

---
+++ login_mysql.php
@@ -0,0 +1,40 @@
+<?php 
+
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'id18369801_root');
+define('DB_PASSWORD', '10Mu32r4****');
+define('DB_NAME', 'id18369801_naizak');
+ 
+/* Attempt to connect to MySQL database */
+$con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+    $username = $_POST['username'];  
+    $password = $_POST['password'];  
+      
+        //to prevent from mysqli injection  
+        $username = stripcslashes($username);  
+        $password = stripcslashes($password);  
+        $username = mysqli_real_escape_string($con, $username);  
+        $password = mysqli_real_escape_string($con, $password);  
+      
+        $sql = "select * from users where username = '$username' and password = '$password'";  
+        $result = mysqli_query($con, $sql);  
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
+        $count = mysqli_num_rows($result);  
+          
+        if($count == 1){  
+            header("location: index_main.php");
+            exit();
+  
+        }  
+        else{
+            header("location: Add_user.php");
+            exit();
+
+        }     
+?>  
