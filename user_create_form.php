87af5110838c7c4742b7c10dff17376adefd7b83
alramama <74817864+alramama@users.noreply.github.com>
2022-02-12 19:14:38 +0300

    Update user_create_form.php

--- user_create_form.php
+++ user_create_form.php
@@ -13,7 +13,7 @@
 <div class="container">
   <h2> Add New User </h2>
   <br>
-  <form method="post" action="Add_user_mysql.php" href="index.php" class="needs-validation" novalidate>
+  <form method="post" action="user_connect_mysql.php" href="index.php" class="needs-validation" novalidate>
     <div class="form-group">
       <label for="username">username</label>
       <input type="text" class="form-control" id="username" placeholder="Enter username" name="username" required>
@@ -32,6 +32,49 @@
       <div class="valid-feedback">Valid.</div>
       <div class="invalid-feedback">Please fill out this field.</div>
     </div>
+        <div class="form-group">
+      <label for="NAME">NAME</label>
+      <input type="text" class="form-control" id="NAME" placeholder="Enter NAME" name="NAME" required>
+      <div class="valid-feedback">Valid.</div>
+      <div class="invalid-feedback">Please fill out this field.</div>
+    </div>
+    <div class="form-group">
+      <label for="Eqama_no">Eqama no</label>
+      <input type="number" class="form-control" id="Eqama_no" placeholder="Enter Eqama_no" name="Eqama_no" required>
+      <div class="valid-feedback">Valid.</div>
+      <div class="invalid-feedback">Please fill out this field.</div>
+    </div>
+    <div class="form-group">
+      <label for="Eqama_Expire_date">Eqama Expire date</label>
+      <input type="date" class="form-control" id="Department" placeholder="Enter Eqama Expire date" name="Eqama_Expire_date" required>
+      <div class="valid-feedback">Valid.</div>
+      <div class="invalid-feedback">Please fill out this field.</div>
+    </div>
+    <div class="form-group">
+      <label for="Birthday">Birthday</label>
+      <input type="date" class="form-control" id="Birthday" placeholder="Enter Birthday" name="Birthday" required>
+      <div class="valid-feedback">Valid.</div>
+      <div class="invalid-feedback">Please fill out this field.</div>
+    </div>
+    <div class="form-group">
+      <label for="Blood_Group">Blood Group</label>
+      <input type="text" class="form-control" id="Blood_Group" placeholder="Enter Blood Group" name="Blood_Group" required>
+      <div class="valid-feedback">Valid.</div>
+      <div class="invalid-feedback">Please fill out this field.</div>
+    </div>
+    <div class="form-group">
+      <label for="job title Iqama">Job Title Iqama</label>
+      <input type="text" class="form-control" id="job title Iqama" placeholder="Enter Job Title Iqama" name="job_title_Iqama" required>
+      <div class="valid-feedback">Valid.</div>
+      <div class="invalid-feedback">Please fill out this field.</div>
+    </div>
+   <div class="form-group">
+      <label for="job title office">Job Title Office</label>
+      <input type="text" class="form-control" id="job_title_office" placeholder="Enter Job Title Office" name="job_title_office" required>
+      <div class="valid-feedback">Valid.</div>
+      <div class="invalid-feedback">Please fill out this field.</div>
+    </div>
+
 
     <button type="submit" name="save" value="submit" class="btn btn-primary" >Submit</button>
 
