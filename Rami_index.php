59fabc5e62837a217867e4e43274b18251785329
alramama <74817864+alramama@users.noreply.github.com>
2022-02-23 11:54:53 +0300

    Create Rami_index.php

---
+++ Rami_index.php
@@ -0,0 +1,67 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'id18369801_root');
+define('DB_PASSWORD', '10Mu32r4****');
+define('DB_NAME', 'id18369801_naizak');
+ 
+/* Attempt to connect to MySQL database */
+$con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+// fetch files
+$sql = "select filename from tbl_files";
+$result = mysqli_query($con, $sql);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Upload View & Download file in PHP and MySQL | Demo</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" >
+    <link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+</head>
+<body>
+    <div class="row">
+        <div class="col-xs-8 col-xs-offset-2">
+            <table class="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Bid name</th>
+                        <th>Reference number</th>
+                        <th>End user</th>
+                        <th>Deadline</th>
+                        <th>File Name</th>
+                        <th>View</th>
+                        <th>Download</th>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php
+                $i = 1;
+                while($row = mysqli_fetch_array($result)) { ?>
+                <tr>
+                    <td><?php echo $i++; ?></td>
+                    <td><?php echo $row['Bid_name']; ?></td>
+                    <td><?php echo $row['Reference_number']; ?></td>
+                    <td><?php echo $row['End_user']; ?></td>
+                    <td><?php echo $row['Deadline']; ?></td>
+                    <td><?php echo $row['filename']; ?></td>
+                    <td><a href="uploads/<?php echo $row['filename']; ?>" target="_blank">View</a></td>
+                    <td><a href="uploads/<?php echo $row['filename']; ?>" download>Download</td>
+                </tr>
+                <?php } ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
