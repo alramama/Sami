3df545d78e0c3d934399db59fad3546f43ac76dc
alramama <74817864+alramama@users.noreply.github.com>
2022-02-23 12:09:48 +0300

    Update Rami_insert.php

--- Rami_insert.php
+++ Rami_insert.php
@@ -11,23 +11,23 @@
 </head>
 <body>
 <div class="container">
-  <h2>Form EXPENSE CLAIM </h2>
+  <h2>Biding>
   <form action="uploads.php" method="post" enctype="multipart/form-data">
     <div class="form-group">
-      <label for="NAME">NAME</label>
-      <input type="text" class="form-control" id="NAME" placeholder="Enter NAME" name="NAME" required>
+      <label for="Bid_name">Bid_name</label>
+      <input type="text" class="form-control" id="Bid_name" placeholder="Enter Bid_name" name="Bid_name" required>
       <div class="valid-feedback">Valid.</div>
       <div class="invalid-feedback">Please fill out this field.</div>
     </div>
     <div class="form-group">
-      <label for="Emp_No">Emp_No</label>
-      <input type="Number" class="form-control" id="Emp_No" placeholder="Enter Emp_No" name="Emp_No" required>
+      <label for="Bid_No">Bid_No</label>
+      <input type="Number" class="form-control" id="Bid_No" placeholder="Enter Bid_No" name="Bid_No" required>
       <div class="valid-feedback">Valid.</div>
       <div class="invalid-feedback">Please fill out this field.</div>
     </div>
     <div class="form-group">
-      <label for="Date">Date</label>
-      <input type="text" class="form-control" id="Date" placeholder="Enter Date" name="Date" required>
+      <label for="Reference_number">Reference_number</label>
+      <input type="text" class="form-control" id="Reference_number" placeholder="Enter Date" name="Reference_number" required>
       <div class="valid-feedback">Valid.</div>
       <div class="invalid-feedback">Please fill out this field.</div>
     </div>
@@ -38,46 +38,63 @@
       <div class="invalid-feedback">Please fill out this field.</div>
     </div>
     <div class="form-group">
-      <label for="Project">Project</label>
-      <input type="text" class="form-control" id="Project" placeholder="Enter Project" name="Project" required>
+      <label for="Duration_of_contract">Duration_of_contract</label>
+      <input type="text" class="form-control" id="Duration_of_contract" placeholder="Enter Duration_of_contract" name="Duration_of_contract" required>
       <div class="valid-feedback">Valid.</div>
       <div class="invalid-feedback">Please fill out this field.</div>
     </div>
    <div class="form-group">
-      <label for="Transfer_sleep">Transfer_sleep</label>
-      <input type="file" class="form-control" id="Transfer_sleep" placeholder="Enter Transfer_sleep" name="Transfer_sleep" required>
+      <label for="End_user">End_user</label>
+      <input type="file" class="form-control" id="End_user" placeholder="Enter End_user" name="End_user" required>
       <div class="valid-feedback">Valid.</div>
       <div class="invalid-feedback">Please fill out this field.</div>
     </div>
-    <button type="submit" name="save" value="submit" class="btn btn-primary">Submit</button>
+    <div class="form-group">
+      <label for="Last_date_inquiries">Last_date_inquiries</label>
+      <input type="file" class="form-control" id="Last_date_inquiries" placeholder="Enter Last_date_inquiries" name="Last_date_inquiries" required>
+      <div class="valid-feedback">Valid.</div>
+      <div class="invalid-feedback">Please fill out this field.</div>
+    </div>
+   <div class="form-group">
+      <label for="End_user">End_user</label>
+      <input type="file" class="form-control" id="End_user" placeholder="Enter End_user" name="End_user" required>
+      <div class="valid-feedback">Valid.</div>
+      <div class="invalid-feedback">Please fill out this field.</div>
+    </div>
+   <div class="form-group">
+      <label for="Deadline">End_user</label>
+      <input type="file" class="form-control" id="Deadline" placeholder="Enter Deadline" name="Deadline" required>
+      <div class="valid-feedback">Valid.</div>
+      <div class="invalid-feedback">Please fill out this field.</div>
+    </div>
+   <div class="form-group">
+      <label for="Bid_opening_date ">End_user</label>
+      <input type="file" class="form-control" id="Bid_opening_date " placeholder="Enter Bid_opening_date " name="Bid_opening_date" required>
+      <div class="valid-feedback">Valid.</div>
+      <div class="invalid-feedback">Please fill out this field.</div>
+    </div>
+   <div class="form-group">
+      <label for="filename ">filename</label>
+      <input type="file" class="form-control" id="Bid_opening_date " placeholder="Enter Bid_opening_date " name="file1" required>
+      <div class="valid-feedback">Valid.</div>
+      <div class="invalid-feedback">Please fill out this field.</div>
+    </div>
+    <input type="submit" name="submit" value="Upload" class="btn btn-primary">Submit</button>
   </form>
 </div>
+<?php if(isset($_GET['st'])) { ?>
+<div class="alert alert-danger text-center">
+<?php if ($_GET['st'] == 'success') 
+{
+  echo "File Uploaded Successfully!";
+}
+else
+  {
+  echo 'Invalid File Extension!';
+  } ?>
+  </div>
+  <?php } ?>
+
 </body>
 </html>
 
-<br/>
-<div class="container">
-    <div class="row">
-        <div class="col-xs-8 col-xs-offset-2 well">
-        <form action="uploads.php" method="post" enctype="multipart/form-data">
-            <legend>Select File to Upload:</legend>
-            <div class="form-group">
-                <input type="file" name="file1" />
-            </div>
-            <div class="form-group">
-                <input type="submit" name="submit" value="Upload" class="btn btn-info"/>
-            </div>
-            <?php if(isset($_GET['st'])) { ?>
-                <div class="alert alert-danger text-center">
-                <?php if ($_GET['st'] == 'success') {
-                        echo "File Uploaded Successfully!";
-                    }
-                    else
-                    {
-                        echo 'Invalid File Extension!';
-                    } ?>
-                </div>
-            <?php } ?>
-        </form>
-        </div>
-    </div>
